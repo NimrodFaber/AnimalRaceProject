7b934d852bfd3459370540f3d50726dc09668162
nimrodfaber <ngf1106@gmaile.com>
2021-11-18 19:46:54 +0200

    init

---
+++ animals.js
@@ -0,0 +1,31 @@
+let runners = {
+    dog: {
+        "name": "dog",
+        "id": "dog",
+        "voice": "woof",
+        "img": "dog.gif",
+        "step": 50
+    },
+    horse: {
+        "name": "horse",
+        "id": "horse",
+        "voice": "neigh",
+        "img": "horse.gif",
+        "step": 70
+    },
+    duck: {
+        "name": "duck",
+        "id": "duck",
+        "voice": "quack",
+        "img": "duck.gif",
+        "step": 40
+    },
+    chick: {
+        "name": "chick",
+        "id": "chick",
+        "voice": "cheap",
+        "img": "chick.gif",
+        "step": 30
+    }
+};
+
